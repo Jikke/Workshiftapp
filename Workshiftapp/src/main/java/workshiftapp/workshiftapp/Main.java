6159a4943f81c4d31d0852c19a89a9a2e54cb5d1
Jirko Leivo <jirko.leivo@gmail.com>
2020-11-12 18:17:35 +0200

    Siistitty repositoriota

---
+++ Workshiftapp/src/main/java/workshiftapp/workshiftapp/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package workshiftapp.workshiftapp;
+import java.util.*;
+
+/**
+ *
+ * @author jeoleivo
+ */
+public class Main {
+    
+    public static void main(String[] args){
+    
+    Menu menu = new Menu();
+    menu.display();
+    
+    }
+}

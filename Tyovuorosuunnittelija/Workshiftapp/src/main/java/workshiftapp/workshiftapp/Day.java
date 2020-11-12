faf6b0732b0afdd6d2283bc2bff4e6cc16bbc7d0
Jirko Leivo <jirko.leivo@gmail.com>
2020-11-12 09:46:05 +0200

    Luotu projekti Workshiftapp

---
+++ Tyovuorosuunnittelija/Workshiftapp/src/main/java/workshiftapp/workshiftapp/Day.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package workshiftapp.workshiftapp;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author jeoleivo
+ */
+public class Day {
+    
+    private ArrayList<Person> morning;
+    private ArrayList<Person> evening;
+    
+}
